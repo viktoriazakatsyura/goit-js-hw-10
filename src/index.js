3557215ef777946e39fc8da49daf6a234439f4c3
Viktoriazakatsyura <viktoriazakatsyura@gmail.com>
2023-01-18 00:44:17 +0200

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,77 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+input.addEventListener('input', debounce(countryInput, DEBOUNCE_DELAY));
+
+function countryInput() {
+  const name = input.value.trim();
+  if (name === '') {
+    return (countryList.innerHTML = ''), (countryInfo.innerHTML = '');
+  }
+
+  fetchCountries(name)
+    .then(countries => {
+      countryList.innerHTML = '';
+      countryInfo.innerHTML = '';
+      if (countries.length === 1) {
+        countryList.insertAdjacentHTML(
+          'beforeend',
+          renderCountryList(countries)
+        );
+        countryInfo.insertAdjacentHTML(
+          'beforeend',
+          renderCountryInfo(countries)
+        );
+      } else if (countries.length >= 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else {
+        countryList.insertAdjacentHTML(
+          'beforeend',
+          renderCountryList(countries)
+        );
+      }
+    })
+    .catch(error =>
+      Notiflix.Notify.failure('Oops, there is no country with that name')
+    );
+}
+
+function renderCountryList(countries) {
+  const markup = countries
+    .map(({ name, flags }) => {
+      return `
+        <li class="country-item">
+         <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
+        <h2>${name.official}</h2>
+        </li>`;
+    })
+    .join('');
+  return markup;
+}
+
+function renderCountryInfo(countries) {
+  const markup = countries
+    .map(({ capital, population, languages }) => {
+      return `
+      <ul class="country-info__list">
+        <li class="country-info__item"><p><b>Capital:</b>${capital}</p></li>
+        <li class="country-info__item"><p><b>Population:</b>${population}</p></li>
+        <li class="country-info__item"><p><b>Capital:</b>${Object.values(
+          languages
+        ).join(',')}</p>
+        </li>
+        </ul>`;
+    })
+    .join('');
+  return markup;
+}
